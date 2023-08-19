e4374027a989fe6b437530822f1103e478ea9f7c
Mateusz <sosnowskimateusz25@gmail.com>
2023-08-19 12:34:37 +0200

    Adding getting accounts for user, and delete account endpoints

--- src/models/account/controller/account.controller.ts
+++ src/models/account/controller/account.controller.ts
@@ -1,8 +1,8 @@
-import {Body, Controller, Post, Req, UseGuards} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Req, UseGuards} from '@nestjs/common';
 import {Request} from "express";
 import {AccountService} from "../service/account.service";
 import {AuthorizationGuard} from "../../../authorization/authorization.guard";
-import {CreateAccountDTO} from "../DTO/account.dto";
+import {CreateAccountDTO} from "../DTO/create-account.dto";
 
 @Controller('account')
 export class AccountController {
@@ -14,7 +14,22 @@ export class AccountController {
     @Post()
     createAccount(@Body() account: CreateAccountDTO, @Req() request: Request) {
         const userId = request.auth.payload.sub;
-
         return this.accountService.createAccount(account, userId);
     }
+
+    @UseGuards(AuthorizationGuard)
+    @Get('/user-accounts')
+    getAccountsForUser(@Req() request: Request){
+        const userId = request.auth.payload.sub;
+
+        return this.accountService.getAccountsForUser(userId);
+    }
+    @UseGuards(AuthorizationGuard)
+    @Delete('/:id')
+    deleteAccountById(@Req() request: Request, @Param('id') id){
+        const userId = request.auth.payload.sub;
+
+        return this.accountService.deleteAccountById(userId, id);
+    }
+
 }
