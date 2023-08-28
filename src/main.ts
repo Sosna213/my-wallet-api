e75e347262476057b49e3fdef3da85abd9158b18
Mateusz <sosnowskimateusz25@gmail.com>
2023-08-28 18:46:18 +0200

    Corrections in delete and create account api

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: configService.get<string>('CLIENT_ORIGIN_URL'),
-    methods: ['GET'],
+    methods: ['GET', 'DELETE'],
     allowedHeaders: ['Authorization', 'Content-Type'],
     maxAge: 86400,
   });
